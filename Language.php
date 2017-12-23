ffc30afff80b95b5844634c87d1723fc6f3d23be
aripdjanov <a.abdualiym@gmail.com>
2017-12-23 13:49:44 +0500

    Initial commit

---
+++ Language.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace aripdjanov\languageClass;
+
+class Language
+{
+    public static function getLangByPrefix(string $prefix) : array
+    {
+        $langList = self::langList();
+        for ($i = 0; $i < count($langList); $i++) {
+            if ($langList[$i]['prefix'] === $prefix) {
+                return $langList[$i];
+            }
+        }
+    }
+
+    public static function getPrefixesLangForCodemix() : array
+    {
+        $langList = self::langList();
+        $langPrefixes = array();
+        for ($i = 0; $i < count($langList); $i++) {
+            $langPrefixes[] = $langList[$i]['prefix'];
+        }
+        return $langPrefixes;
+    }
+
+
+    public static function langList() : array
+    {
+        return [
+            [
+                'id' => '1',
+                'prefix' => 'en',
+                'title' => 'English'
+            ],
+            [
+                'id' => '2',
+                'prefix' => 'ru',
+                'title' => 'Русский'
+            ],
+            [
+                'id' => '3',
+                'prefix' => 'oz',
+                'title' => 'O`zbekcha'
+            ],
+            [
+                'id' => '4',
+                'prefix' => 'uz',
+                'title' => 'Ўзбекча'
+            ],
+        ];
+    }
+}
